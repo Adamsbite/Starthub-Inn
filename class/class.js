91c7e9b62efa7e732823d651ea72c25193c8d63c
bright adams <btadamz@gmail.com>
2024-06-14 16:25:16 +0100

    last

---
+++ class/class.js
@@ -0,0 +1,9 @@
+var Human = /** @class */ (function () {
+    function Human(name, gender, age, isHuman) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.isHuman = isHuman;
+    }
+    return Human;
+}());
--- class/class.ts
+++ class/class.ts
@@ -10,3 +10,4 @@ class Human {
         this.age = age;
         this.isHuman = isHuman;
 }
+}
