f9d0bf6108421520e481cb7b82c5cbe84bd3acd5
bright adams <btadamz@gmail.com>
2024-06-14 16:16:23 +0100

    last

---
+++ class/class.ts
@@ -0,0 +1,12 @@
+class Human {
+    name: string;
+    gender: string;
+    age: number;
+    isHuman: boolean;
+
+    constructor(name: string, gender: string, age: number, isHuman: boolean) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.isHuman = isHuman;
+}
