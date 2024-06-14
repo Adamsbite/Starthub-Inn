0de97ed1502a2cbec383794456ff06cab9608b66
bright adams <btadamz@gmail.com>
2024-06-14 17:13:48 +0100

    last

--- class/class.js
+++ class/class.js
@@ -5,5 +5,11 @@ var Human = /** @class */ (function () {
         this.age = age;
         this.isHuman = isHuman;
     }
+    Human.prototype.person = function () {
+        return ('Hi my name is ' + this.name + ' and i am a ' + this.gender + 'AND i am a ' + this.isHuman);
+    };
     return Human;
 }());
+var people = new Human('bite', 'nale', 11, true);
+// console.log(people);
+console.log(people.person());
--- class/class.ts
+++ class/class.ts
@@ -1,13 +1,51 @@
-class Human {
-    name: string;
-    gender: string;
-    age: number;
-    isHuman: boolean;
+// class explanation
 
-    constructor(name: string, gender: string, age: number, isHuman: boolean) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.isHuman = isHuman;
+class Human {
+  name: string;
+  gender: string;
+  age: number;
+  isHuman: boolean;
+
+  constructor(name: string, gender: string, age: number, isHuman: boolean) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.isHuman = isHuman;
+  }
+
+  //   this is a function
+  person() {
+    return (
+      "Hi my name is " +
+      this.name +
+      " and i am a " +
+      this.gender +
+      "AND i am a " +
+      this.isHuman
+    );
+  }
 }
+const people = new Human("bite", "nale", 11, true);
+//    console.log(people);
+// console.log(people.person())
+
+// inheritance class
+class bite extends Human {
+  constructor(name: string, gender: string, age: number, isHuman: boolean) {
+    super(name, gender, age, isHuman);
+  }
+  child() {
+    return (
+      "Hi my name is " +
+      this.name +
+      " and i am a " +
+      this.gender +
+      "AND i am a " +
+      this.isHuman
+    );
+  }
 }
+
+const pikin = new bite("bite", "nale", 11, true);
+console.log(pikin);
+console.log(pikin.child);
